006bf0319ff0daaa62d159937f0ecb5534ed5cbf
lyyyuna <lyyyuna@gmail.com>
2014-02-15 11:18:48 +0800

    memory controller

---
+++ crowbar/mem_control/mem_control.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mem_control.h"
+
+static void default_error_handler(MEM_Controller);
---
+++ crowbar/mem_control/mem_control.h
@@ -0,0 +1,13 @@
+#ifndef MEM_H_INCLUDED
+#define MEM_H_INCLUDED
+#include "mem.h"
+
+typedef union Header_tag Header;
+
+struct MEM_Controller_tag {
+    FILE                *error_fp;
+    MEM_ErrorHandler    error_handler;
+    MEM_FailMode        fail_mode;
+    Header              *block_header;
+};
+#endif
